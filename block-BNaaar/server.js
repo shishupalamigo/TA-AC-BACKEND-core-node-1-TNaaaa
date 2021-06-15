77866cec18cae896322724203c33aebbe13fe2da
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-06-15 22:59:06 +0530

    Handle Multiple requests

---
+++ block-BNaaar/server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer(handleRequest);
+
+function handleRequest (req, res) {
+    let parsedUrl = url.parse(req.url);
+    let pathName = parsedUrl.pathname;
+    if (req.method === 'GET' && pathName === '/') {
+        res.end('Welcome to homepage')
+    } else if (req.method === 'GET' && pathName === '/about') {
+     res.writeHead(200, {'Content-Type': 'text/html'});
+     res.end(`<h2>This is all about NodeJS</h2>`);   
+    } else if (req.method === 'POST' && pathName === '/about') {
+        res.writeHead(200, {'Content-Type': 'application/JSON'});
+        res.write(`{message: this is a post request}`);
+        res.end();
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end(`<h2>Error 404 :Page Not found!</h2>`); 
+    }
+}
+
+server.listen(5000, () => {
+    console.log('localhost listening to port 5k');
+});
